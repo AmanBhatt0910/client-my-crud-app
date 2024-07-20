253bdd523c6b5dddb1265c129182a29f916fd190
atulRaaj <atulraj1010@tutanota.com>
2024-07-20 08:41:06 +0530

    Added new entries, updated login redirect

--- app/admin/register/page.jsx
+++ app/admin/register/page.jsx
@@ -1,4 +1,4 @@
-import RegisterForm from "../components/RegisterForm";
+import RegisterForm from "../../components/RegisterForm";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
